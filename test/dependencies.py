312a1a4c80582af699fc82f5e739b7225f652cf1
Horikita2023 <129011030+Horikita2023@users.noreply.github.com>
2023-06-02 15:29:56 +1000

    Add files via upload

---
+++ test/dependencies.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import requests
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import geopandas as gpd
+import json
+from geopy.distance import geodesic
+import datetime
+
